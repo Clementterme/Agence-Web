f7764271e3d65517bfcff630cc5be5f4e2fa3ff3
feras <feras.altalib@gmail.com>
2024-01-19 16:21:01 +0100

    footerRESPONSIVE

---
+++ script.js
@@ -0,0 +1,7 @@
+function join_us (){
+    let message = "join us";
+    let para_facebook = document.getElementById('para_facebook');
+    para_facebook.classList.remove('invisible');
+    para_facebook.classList.add('visible');
+    
+} 
